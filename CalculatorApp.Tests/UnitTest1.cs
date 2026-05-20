918b5bf415544bb6e4129e420a505229987179ca
Matvej Moschkof <matvejmoschkoff2007@gmail.com>
2026-05-20 15:56:20 +0300

    Initial commit

---
+++ CalculatorApp.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace CalculatorApp.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_ReturnsCorrectSum()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+            int result = calc.Add(2, 3);
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+    }
+}
---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calculator();
+            Console.WriteLine(calc.Add(5, 3));
+        }
+    }
+}
